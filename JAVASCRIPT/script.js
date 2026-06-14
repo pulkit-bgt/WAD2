4d95a6ed8a2ba5271685a0c301c00cf34c5116bf
Pulkit <pulkitbhagat349@gmail.com>
2026-06-14 14:13:08 +0530

    development

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -2,3 +2,29 @@
 
 document.querySelector(".login-btn").addEventListener("click", () => {
     alert("Redirecting to Login Page..
+
+        
+
+document.querySelector(".login-btn").addEventListener("click", () => {
+    alert("Redirecting to Login Page..
+
+function togglePassword(){
+
+    let password =
+    document.getElementById("password");
+
+    password.style.opacity="0.5";
+
+    setTimeout(() => {
+
+        if(password.type==="password"){
+            password.type="text";
+        }
+        else{
+            password.type="password";
+        }
+
+        password.style.opacity="1";
+
+    },150);
+}
