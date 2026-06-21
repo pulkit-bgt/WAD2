6f8676086e7eaae195452912c5f920423394a412
Pulkit <pulkitbhagat349@gmail.com>
2026-06-21 20:49:15 +0530

    Update script.js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -542,3 +542,14 @@ function displayUsersInAdmin()
   });
 }
 
+
+/* BUTTONS */
+
+
+document.getElementById("registerBtn").addEventListener("click", function () {
+    window.location.href = "register.html";
+});
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+    window.location.href = "login.html";
+});
