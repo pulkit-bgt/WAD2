f4676901db92fbabd0f2d419f5b03e772fd349f3
Harshit-pratik01 <kujur310301@gmail.com>
2026-06-21 21:08:08 +0530

    Update script.js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -565,10 +565,12 @@ function deleteUser(index)
 /* BUTTONS */
 
 
-document.getElementById("registerBtn").addEventListener("click", function () {
+document.getElementById("registerBtn").addEventListener("click", function () 
+{
     window.location.href = "register.html";
 });
 
-document.getElementById("loginBtn").addEventListener("click", function () {
+document.getElementById("loginBtn").addEventListener("click", function () 
+{
     window.location.href = "login.html";
 });
